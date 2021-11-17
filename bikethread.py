12b378bc96d69f7d76962ada17ec3d3c3b5e07c9
Rilr20 <nrrl99@gmail.com>
2021-11-17 21:26:05 +0100

    added and commented out user functions

--- bikethread.py
+++ bikethread.py
@@ -5,12 +5,13 @@ class bikeThread(Thread):
     """
     bike thread class where it runs creates a thread and runs an update on bikes
     """
-    def __init__(self, bikes):
+    def __init__(self, bikes):#,users):
         """
         thread constructor 
         """
         Thread.__init__(self)
         self.bikelist = bikes
+        # self.userlist = userss
         self._running = True
     
     def run(self):
@@ -18,10 +19,11 @@ class bikeThread(Thread):
         starts a thread
         """
         while self._running:
+            print("arg")
             # print(f'running update thread {self.getName()}')
+            # self.update_users()
             self.update_bikes()
-
-            time.sleep(10) #pauses for 1 min
+            time.sleep(10) #pauses for 10seconds
     def terminate(self):
         """
         stops the thread
@@ -33,7 +35,9 @@ class bikeThread(Thread):
         runs the updatePos method on bikes where upptagen is true
         """
         for bike in self.bikelist:
+            # print(bike.status)
             if bike.status == 'unavailable':
+                # print("biketime")
                 bike.updatePos()
                 bike.bikeprint()
                 bike.putRequest()
@@ -44,3 +48,7 @@ class bikeThread(Thread):
                 bike.putRequest
                 print(f'{bike._id} updated')
 
+    def update_users(self):
+        pass
+        # for user in self.userlist:
+        #     user.decreasewait()
