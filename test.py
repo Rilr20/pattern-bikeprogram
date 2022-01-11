8d659d70a6838529a62f9448db22b4b2811545a2
Rilr20 <nrrl99@gmail.com>
2022-01-11 14:31:56 +0100

    removed tests that used the restAPI

--- test.py
+++ test.py
@@ -6,68 +6,69 @@ unittest test
 # from unittest import mock
 from unittest.case import TestCase
 import api
-import main
+# import main
 from bike import Bike
 from user import User
 
-class TestAPI(TestCase):
-    """Unittest API submodule"""
-    def test_init(self):
-        """init test"""
-        self.assertEqual(api.PORT, "8000")
-        self.assertEqual(api.API_URL, f'http://localhost:{api.PORT}/sparkapi/v1/')
-        self.assertIsNotNone(api.CITIES)
+#USES API
+# class TestAPI(TestCase):
+#     """Unittest API submodule"""
+#     def test_init(self):
+#         """init test"""
+#         self.assertEqual(api.PORT, "8000")
+#         self.assertEqual(api.API_URL, f'http://localhost:{api.PORT}/sparkapi/v1/')
+#         self.assertIsNotNone(api.CITIES)
 
-    def test_city_check(self):
-        """
-        tests the city_check function
-        """
-        test_array = [[0.025, 0.035, 1], [1000, 0, False]]
-        for item in test_array:
-            # print(item)
-            res = api.city_check(item[0], item[1])
-            # print(res)
-            self.assertEqual(res, item[2])
+#     def test_city_check(self):
+#         """
+#         tests the city_check function
+#         """
+#         test_array = [[0.025, 0.035, 1], [1000, 0, False]]
+#         for item in test_array:
+#             # print(item)
+#             res = api.city_check(item[0], item[1])
+#             # print(res)
+#             self.assertEqual(res, item[2])
 
-    def test_parking_check(self):
-        """
-        testing the parking_check function
-        """
-        test_array = [[-0.025,-0.025, 1], [1000, 0, False]]
-        for item in test_array:
-            # print(item)
-            res = api.parking_check(item[0], item[1])
-            # print(res)
-            self.assertEqual(res, item[2])
+#     def test_parking_check(self):
+#         """
+#         testing the parking_check function
+#         """
+#         test_array = [[-0.025,-0.025, 1], [1000, 0, False]]
+#         for item in test_array:
+#             # print(item)
+#             res = api.parking_check(item[0], item[1])
+#             # print(res)
+#             self.assertEqual(res, item[2])
 
-    def test_charging_check(self):
-        """
-        testing the charging_check function
-        """
-        test_array = [[0.025,0.025, 1], [1000, 0, False]]
-        for item in test_array:
-            # print(item)
-            res = api.charging_check(item[0], item[1])
-            # print(res)
-            self.assertEqual(res, item[2])
+#     def test_charging_check(self):
+#         """
+#         testing the charging_check function
+#         """
+#         test_array = [[0.025,0.025, 1], [1000, 0, False]]
+#         for item in test_array:
+#             # print(item)
+#             res = api.charging_check(item[0], item[1])
+#             # print(res)
+#             self.assertEqual(res, item[2])
 
-    def test_inside_circle(self):
-        """
-        testing to see if point is inside a circle
-        """
-        circle_variable = (0,0,5)
-        point = [(0,0),(-1,-0.5)]
-        for item in point:
-            result = api.inside_circle(
-                    circle_variable[0],
-                    circle_variable[1],
-                    circle_variable[2],
-                    item[0],
-                    item[1]
-                )
-            self.assertEqual(result,True)
-            result = api.inside_circle(10, 10,circle_variable[2],item[0],item[1])
-            self.assertEqual(result,False)
+#     def test_inside_circle(self):
+#         """
+#         testing to see if point is inside a circle
+#         """
+#         circle_variable = (0,0,5)
+#         point = [(0,0),(-1,-0.5)]
+#         for item in point:
+#             result = api.inside_circle(
+#                     circle_variable[0],
+#                     circle_variable[1],
+#                     circle_variable[2],
+#                     item[0],
+#                     item[1]
+#                 )
+#             self.assertEqual(result,True)
+#             result = api.inside_circle(10, 10,circle_variable[2],item[0],item[1])
+#             self.assertEqual(result,False)
 
 class TestBike(TestCase):
     """
@@ -108,14 +109,15 @@ class TestBike(TestCase):
             testbike.y_pos = 0
             count += 1
 
-    def test_move_to_city(self):
-        """
-        test to move bikes to city if outside border
-        """
-        testbike = Bike(1000,1000,0,100, "available")
-        testbike.move_to_city(False)
-        self.assertEqual(testbike.x_pos,0)
-        self.assertEqual(testbike.y_pos,0)
+    #USES API
+    # def test_move_to_city(self):
+    #     """
+    #     test to move bikes to city if outside border
+    #     """
+    #     testbike = Bike(1000,1000,0,100, "available")
+    #     testbike.move_to_city(False)
+    #     self.assertEqual(testbike.x_pos,0)
+    #     self.assertEqual(testbike.y_pos,0)
 
     def test_increase_velocity(self):
         """
@@ -148,61 +150,66 @@ class TestBike(TestCase):
             count += 1
         self.assertEqual(testbike.status, "available")
 
-    def test_in_centrum(self):
-        """
-        testing velocity changes while in centrum of city
-        """
-        testbike = Bike(0,0,0,100,"available")
-        #max speed increasing should become second slowest
-        testbike.increase_velocity(3)
-        self.assertEqual(testbike.velocity, 0.001383)
-        #max speed decreaseing hould become second slowest
-        testbike.decrease_velocity(3)
-        self.assertEqual(testbike.velocity, 0.001383)
-        #slowest speed should stop in centrum
-        testbike.decrease_velocity(0)
-        self.assertEqual(testbike.velocity, 0)
+    #USES API
+    # def test_in_centrum(self):
+    #     """
+    #     testing velocity changes while in centrum of city
+    #     """
+    #     testbike = Bike(0,0,0,100,"available")
+    #     #max speed increasing should become second slowest
+    #     testbike.increase_velocity(3)
+    #     self.assertEqual(testbike.velocity, 0.001383)
+    #     #max speed decreaseing hould become second slowest
+    #     testbike.decrease_velocity(3)
+    #     self.assertEqual(testbike.velocity, 0.001383)
+    #     #slowest speed should stop in centrum
+    #     testbike.decrease_velocity(0)
+    #     self.assertEqual(testbike.velocity, 0)
 
-    def test_move_to_charging(self):
-        """
-        testing move bike to a charging station
-        """
-        testbike = Bike(0,0,0,10,"available")
-        testbike.move_to_charging()
-        self.assertEqual(testbike.x_pos, 0.025)
-        self.assertEqual(testbike.y_pos, 0.025)
+    #USES API
+    # def test_move_to_charging(self):
+    #     """
+    #     testing move bike to a charging station
+    #     """
+    #     testbike = Bike(0,0,0,10,"available")
+    #     testbike.move_to_charging()
+    #     self.assertEqual(testbike.x_pos, 0.025)
+    #     self.assertEqual(testbike.y_pos, 0.025)
 
-    def test_remove_from_charging(self):
-        """
-        testing removing bike from charging station
-        """
-        testbike = Bike(0,0,0,10,"available")
-        testbike.remove_from_charging()
-        self.assertEqual(testbike.x_pos, 0)
-        self.assertEqual(testbike.y_pos, 0)
+    #USES API
+    # def test_remove_from_charging(self):
+    #     """
+    #     testing removing bike from charging station
+    #     """
+    #     testbike = Bike(0,0,0,10,"available")
+    #     testbike.remove_from_charging()
+    #     self.assertEqual(testbike.x_pos, 0)
+    #     self.assertEqual(testbike.y_pos, 0)
 
-    def test_charging(self):
-        """
-        testing charging bike
-        """
-        testbike = Bike(0,0.025,0.025,9,"available")
-        count = 10
-        testbike.charging()
-        self.assertEqual(testbike.status, "charging")
-        while count < 100:
-            count += 1
-            testbike.charging()
-            self.assertEqual(testbike.battery, count)
-        testbike.charging()
-        self.assertEqual(testbike.status, "available")
+    #USES API
+    # def test_charging(self):
+    #     """
+    #     testing charging bike
+    #     """
+    #     testbike = Bike(0,0.025,0.025,9,"available")
+    #     count = 10
+    #     testbike.charging()
+    #     self.assertEqual(testbike.status, "charging")
+    #     while count < 100:
+    #         count += 1
+    #         testbike.charging()
+    #         self.assertEqual(testbike.battery, count)
+    #     testbike.charging()
+    #     self.assertEqual(testbike.status, "available")
 
-    def test_charging_outside_of_station(self):
-        """
-        testing what happens when bike charge outside of charging station
-        """
-        testbike = Bike(0,0,0,9,"charging")
-        testbike.charging()
-        self.assertEqual(testbike.status, "available")
+    #USES API
+    # def test_charging_outside_of_station(self):
+    #     """
+    #     testing what happens when bike charge outside of charging station
+    #     """
+    #     testbike = Bike(0,0,0,9,"charging")
+    #     testbike.charging()
+    #     self.assertEqual(testbike.status, "available")
 
     def test_sercice(self):
         """
@@ -237,15 +244,15 @@ class TestUser(TestCase):
             self.assertEqual(testuser.wait, start-i)
             testuser.decrease_wait()
 
-class TestMain(TestCase):
-    """
-    testing the main file
-    """
-    def test_create_users(self):
-        """
-        testing create user function
-        """
-        length = [1, 5, 10, 0]
-        for i in length:
-            users = main.create_users(i)
-            self.assertEqual(len(users), i)
+# class TestMain(TestCase):
+#     """
+#     testing the main file
+#     """
+#     def test_create_users(self):
+#         """
+#         testing create user function
+#         """
+#         length = [1, 5, 10, 0]
+#         for i in length:
+#             users = main.create_users(i)
+#             self.assertEqual(len(users), i)
